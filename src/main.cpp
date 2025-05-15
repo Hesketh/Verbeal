0a43ecfad448b2dee207269862cd2331020e7a08
Alex Hesketh <alexhesketh@outlook.com>
2025-05-15 21:21:41 +0100

    Add spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Verbeal");
     window.setFramerateLimit(144);
+
+	auto console = spdlog::stdout_color_mt("console");    
+    auto err_logger = spdlog::stderr_color_mt("stderr");    
+    spdlog::get("console")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
 
     while (window.isOpen())
     {
